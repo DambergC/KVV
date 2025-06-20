f09224e40da0ea4777ab56c2d532971ca5137efb
chida <christian@damberg.org>
2025-06-20 22:12:30 +0200

    Update 20250620

---
+++ KVV-Driftstorning/Globals.ps1
@@ -0,0 +1,96 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+function Get-HKLMRegistryValue
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[string]$KeyPath,
+		[Parameter(Mandatory = $false)]
+		[string]$ValueName
+	)
+	
+	try
+	{
+		# Ensure the path starts with HKLM:
+		if (-not $KeyPath.StartsWith("HKLM:\"))
+		{
+			$KeyPath = "HKLM:\$KeyPath"
+		}
+		
+		# Check if the key exists
+		if (-not (Test-Path -Path $KeyPath))
+		{
+			Write-Error "Registry key '$KeyPath' does not exist."
+			return $null
+		}
+		
+		# If a specific value is requested
+		if ($ValueName)
+		{
+			$value = Get-ItemProperty -Path $KeyPath -Name $ValueName -ErrorAction SilentlyContinue
+			if ($value -ne $null)
+			{
+				return $value.$ValueName
+			}
+			else
+			{
+				Write-Error "Value '$ValueName' does not exist in key '$KeyPath'"
+				return $null
+			}
+		}
+		# Return all values in the key
+		else
+		{
+			return Get-ItemProperty -Path $KeyPath
+		}
+	}
+	catch
+	{
+		Write-Error "Error accessing registry: $_"
+		return $null
+	}
+}
+
+# Example usage:
+# Get a specific value from a registry key
+# Get-HKLMRegistryValue -KeyPath "SOFTWARE\Microsoft\Windows NT\CurrentVersion" -ValueName "ProductName"
+
+# Get all values from a registry key
+# Get-HKLMRegistryValue -KeyPath "SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
+
+
+
+
---
+++ KVV-IncidentInfoService/IncidentInfoService/README.md
@@ -0,0 +1,16 @@
+﻿# IncidentInfoService
+--Overview high level of the program.--
+## Description
+--Description of the program.--
+## Getting Started
+--Information about getting the program setup.--
+## Help
+--Additional information need to use the software.--
+## Authors
+Administrator
+## Version History
+--Version information and history.--
+## License
+--Software license information.--
+## Acknowledgments
+--Place any acknowledgments here.--
